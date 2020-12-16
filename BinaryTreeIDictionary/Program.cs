ec0d8388e7dc285edf29b7e30649288a98d0a19a
Raptor3012 <aleksey.shalov@gmail.com>
2020-12-16 16:50:07 +0500

    Добавьте файлы проекта.

---
+++ BinaryTreeIDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTreeIDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
